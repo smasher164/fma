a343b80e651535cc0467f44b97e7758c168f6ca8
Akhil Indurti <aindurti@gmail.com>
2018-07-26 02:16:54 -0400

    Revert ARM implementation until feature detection is known to work.
    SIGILL was not truly being caught. Until feature detection is locked
    down for GOOS=ARM machines with FMA support, we will fallback to a
    software implementation. Testing on master shows that the FMADDD
    instruction is now supported by assembler, so `FMADDD F0, F2, F1, F0`
    executes without WORD directives.
    
    Updates #1.

--- fma.go
+++ fma.go
@@ -1,8 +1,10 @@
 package fma
 
+import "fmt"
+
 func FMA(x, y, z float64) float64
 func fma(x, y, z float64) float64 {
-	print("portable\n")
+	fmt.Println("portable")
 	// TODO replace with software implementation.
 	return 0
 }
