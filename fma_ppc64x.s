84e31318e736c75945cecb90e589715ac74dca0c
Akhil Indurti <aindurti@gmail.com>
2018-07-30 13:33:01 -0400

    Add ppc64x implementation
    This change adds an assembly implementation for ppc64 and ppc64le.
    Because plan 9 assembly has a degree of portability, the implementation
    is identical to arm64's.

---
+++ fma_ppc64x.s
@@ -0,0 +1,12 @@
+// +build ppc64 ppc64le
+
+#include "textflag.h"
+
+// func FMA(x, y, z float64) float64
+TEXT ·FMA(SB),NOSPLIT,$0
+	FMOVD x+0(FP), F0
+	FMOVD y+8(FP), F1
+	FMOVD z+16(FP), F2
+	FMADD F0, F2, F1, F0
+	FMOVD F0, ret+24(FP)
+	RET
